49790f7bf517f273fe4a1623182b0b4985af934a
Hari Nair <harinnair@yahoo.com>
2018-01-10 13:38:46 +0530

    Add files via upload
    moved out the tone generating code from varioaudio.cpp so it can be used independently of the varioaudio module

---
+++ audio.cpp
@@ -0,0 +1,26 @@
+#include "Arduino.h"
+#include "audio.h"
+
+static int pinPWM_;
+
+void audio_Config(int pinPWM) {
+  pinPWM_       = pinPWM;
+  analogWrite(pinPWM_, 0);
+  }
+
+void audio_SetFrequency(int32_t freqHz) {
+	if (freqHz ) {
+		analogWriteFreq(freqHz);
+		analogWrite(pinPWM_, 512); // generate square wave with frequency fHz and 50% duty cycle (512/1023 ~= 0.5)
+		}
+	else {
+		analogWrite(pinPWM_, 0); // turn off output
+		}
+	}
+
+
+void audio_GenerateTone(int32_t freqHz, int ms) {
+    audio_SetFrequency(freqHz);
+    delay(ms);
+    audio_SetFrequency(0);
+    }
